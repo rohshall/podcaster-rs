2e000685b90b2be0c00a4a2f8d417e8b2451fb9a
Salil Wadnerkar <bsdprg@icloud.com>
2024-09-09 20:49:48 -0700

    Refactoring

--- src/actions.rs
+++ src/actions.rs
@@ -5,13 +5,17 @@ use url::Url;
 use std::io::Write;
 use std::io::Read;
 use std::path::{Path, PathBuf};
+use std::collections::HashMap;
 use attohttpc;
 use roxmltree;
 use std::error::Error;
 use crate::common::Episode;
+use colored::Colorize;
+use std::process::{Command, Stdio};
+use crate::config::Podcast;
 
 // Parse the podcast feed to extract information of the episodes.
-pub fn get_episodes(podcast_url: &String, count: usize) -> Result<Vec<Episode>, Box<dyn Error>> {
+fn get_episodes(podcast_url: &String, count: usize) -> Result<Vec<Episode>, Box<dyn Error>> {
     let podcast_response = attohttpc::get(podcast_url).send()?;
     let podcast_feed_contents = podcast_response.text()?;
     let podcast_feed_doc = roxmltree::Document::parse(&podcast_feed_contents)?;
@@ -61,7 +65,7 @@ fn download_episode(url: &str, dir_path: &PathBuf) -> Result<(), Box<dyn Error>>
 }
 
 // Try to download all episodes, and return the list of downloaded episodes.
-pub fn download_podcast(episodes: Vec<Episode>, dir_path: &PathBuf, episodes_downloaded: &Vec<Episode>) -> Result<Vec<Episode>, Box<dyn Error>> {
+fn download_podcast(episodes: Vec<Episode>, dir_path: &PathBuf, episodes_downloaded: &Vec<Episode>) -> Result<Vec<Episode>, Box<dyn Error>> {
     fs::create_dir_all(dir_path)?;
     let guids_downloaded: Vec<&str> = episodes_downloaded.into_iter().map(|e| e.guid.as_str()).collect();
     let downloaded = episodes.into_iter()
@@ -81,7 +85,7 @@ pub fn download_podcast(episodes: Vec<Episode>, dir_path: &PathBuf, episodes_dow
     Ok(downloaded)
 }
 
-pub fn get_episode_download(episode: &Episode, dir_path: &PathBuf) -> Option<PathBuf> {
+fn get_episode_download(episode: &Episode, dir_path: &PathBuf) -> Option<PathBuf> {
     Url::parse(&episode.url.as_str()).ok().and_then(|url| -> Option<PathBuf> {
         let file_name = Path::new(url.path()).file_name().unwrap();
         let path = dir_path.join(file_name);
@@ -91,3 +95,110 @@ pub fn get_episode_download(episode: &Episode, dir_path: &PathBuf) -> Option<Pat
         }
     })
 }
+
+pub fn download_podcasts(podcasts: Vec<Podcast>, media_dir: &Path, count: usize, previous_state: &HashMap<String, Vec<Episode>>) -> HashMap<String, Vec<Episode>> {
+    let no_episodes: Vec<Episode> = Vec::new();
+    podcasts.into_iter()
+        .filter_map(|podcast| {
+            let prev_downloaded_episodes = previous_state.get(&podcast.id).unwrap_or(&no_episodes);
+            let dir_path = media_dir.join(&podcast.id);
+            match get_episodes(&podcast.url, count) {
+                Ok(episodes) => {
+                    match download_podcast(episodes, &dir_path, prev_downloaded_episodes) {
+                        Ok(downloaded_episodes) => {
+                            println!("{:?} {} episodes downloaded", podcast.id, downloaded_episodes.len());
+                            Some((podcast.id, downloaded_episodes))
+                        },
+                        Err(e) => {
+                            eprintln!("Could not download the podcast {}: {}", podcast.id, e);
+                            None
+                        }
+                    }
+                },
+                Err(e) => {
+                    eprintln!("Could not get the podcast feed: {}", e);
+                    None
+                },
+            }
+        }).collect()
+}
+
+pub fn compute_updated_state(new_state: HashMap<String, Vec<Episode>>, previous_state: HashMap<String, Vec<Episode>>) -> HashMap<String, Vec<Episode>> { 
+    let no_episodes: Vec<Episode> = Vec::new();
+    // Merge the previous state into the new state to get the updated current state.
+    // App state consists of what episodes were downloaded for what podcasts.
+    // To avoid storing infinite history, truncate it to latest 100 episodes.
+    let mut updated_state: HashMap<String, Vec<Episode>> = HashMap::new();
+    for (podcast_id, new_episodes) in new_state.into_iter() {
+        let mut new_episodes = new_episodes.clone();
+        let previous_episodes = previous_state.get(&podcast_id).unwrap_or(&no_episodes);
+        new_episodes.extend_from_slice(previous_episodes.as_slice());
+        updated_state.insert(podcast_id, new_episodes);
+    }
+    for (podcast_id, old_episodes) in previous_state.into_iter() {
+        if updated_state.get(&podcast_id).is_none() {
+            updated_state.insert(podcast_id, old_episodes);
+        }
+    }
+    updated_state
+}
+
+pub fn show_remote(podcasts: Vec<Podcast>, count: usize) {
+    for podcast in podcasts.into_iter() {
+        match get_episodes(&podcast.url, count) {
+            Ok(episodes) => {
+                println!("\n{}:", podcast.id.magenta().bold());
+                for episode in episodes.iter() {
+                    println!("{}", episode);
+                }
+            },
+            Err(e) => {
+                eprintln!("Could not get the podcast feed: {}", e);
+            },
+        }
+    }
+}
+
+pub fn show_local(podcasts: Vec<Podcast>, count: usize, previous_state: HashMap<String, Vec<Episode>>) {
+    let no_episodes: Vec<Episode> = Vec::new();
+    for podcast in podcasts.into_iter() {
+        let episodes: Vec<&Episode> = previous_state.get(&podcast.id).unwrap_or(&no_episodes).iter().take(count).collect();
+        if episodes.len() == 0 {
+            println!("\n{}: no episode available. Download it first.", podcast.id.magenta().bold());
+        } else {
+            println!("\n{}:", podcast.id.magenta().bold());
+            for episode in episodes.into_iter() {
+                println!("{}", episode);
+            }
+        }
+    }
+}
+
+pub fn play_podcasts(podcasts: Vec<Podcast>, count: usize, media_dir: &Path, player: String, previous_state: HashMap<String, Vec<Episode>>) {
+    let no_episodes: Vec<Episode> = Vec::new();
+    for podcast in podcasts.into_iter() {
+        let episodes: Vec<&Episode> = previous_state.get(&podcast.id).unwrap_or(&no_episodes).iter().take(count).collect();
+        let dir_path = media_dir.join(&podcast.id);
+        if episodes.len() == 0 {
+            println!("\n{}: no episode available. Download it first.", podcast.id.magenta().bold());
+        } else {
+            println!("\n{}:", podcast.id.magenta().bold());
+            for episode in episodes.into_iter() {
+                match get_episode_download(episode, &dir_path) {
+                    Some(path) => {
+                        let child = Command::new(&player)
+                            .arg(path.display().to_string())
+                            .stdout(Stdio::piped())
+                            .spawn()
+                            .expect("failed to execute the player");
+                        child
+                            .wait_with_output()
+                            .expect("failed to wait on child");
+                        },
+                    None => eprintln!("Could not get the file for the episode at URL {}", episode.url)
+                }
+            }
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,9 @@ mod args;
 mod config;
 mod actions;
 use std::collections::HashMap;
-use colored::Colorize;
 use std::path::Path;
-use std::process::{Command, Stdio};
 use {
     crate::{
-        common::*,
         args::*,
         config::*,
         actions::*,
@@ -42,48 +39,11 @@ fn main() {
 
     match args.action {
         Action::DOWNLOAD => {
-            let no_episodes: Vec<Episode> = Vec::new();
             let podcasts = select_podcasts(settings.podcasts, args.podcast_id);
             let count = args.count.unwrap_or(1);
             let media_dir = Path::new(&settings.config.media_dir);
-            let new_state: HashMap<String, Vec<Episode>> = podcasts.into_iter()
-                .filter_map(|podcast| {
-                    let prev_downloaded_episodes = previous_state.get(&podcast.id).unwrap_or(&no_episodes);
-                    let dir_path = media_dir.join(&podcast.id);
-                    match get_episodes(&podcast.url, count) {
-                        Ok(episodes) => {
-                            match download_podcast(episodes, &dir_path, prev_downloaded_episodes) {
-                                Ok(downloaded_episodes) => {
-                                    println!("{:?} {} episodes downloaded", podcast.id, downloaded_episodes.len());
-                                    Some((podcast.id, downloaded_episodes))
-                                },
-                                Err(e) => {
-                                    eprintln!("Could not download the podcast {}: {}", podcast.id, e);
-                                    None
-                                }
-                            }
-                        },
-                        Err(e) => {
-                            eprintln!("Could not get the podcast feed: {}", e);
-                            None
-                        },
-                    }
-                }).collect();
-            // Merge the previous state into the new state to get the updated current state.
-            // App state consists of what episodes were downloaded for what podcasts.
-            // To avoid storing infinite history, truncate it to latest 100 episodes.
-            let mut updated_state: HashMap<String, Vec<Episode>> = HashMap::new();
-            for (podcast_id, new_episodes) in new_state.into_iter() {
-                let mut new_episodes = new_episodes.clone();
-                let previous_episodes = previous_state.get(&podcast_id).unwrap_or(&no_episodes);
-                new_episodes.extend_from_slice(previous_episodes.as_slice());
-                updated_state.insert(podcast_id, new_episodes);
-            }
-            for (podcast_id, old_episodes) in previous_state.into_iter() {
-                if updated_state.get(&podcast_id).is_none() {
-                    updated_state.insert(podcast_id, old_episodes);
-                }
-            }
+            let new_state = download_podcasts(podcasts, media_dir, count, &previous_state);
+            let updated_state = compute_updated_state(new_state, previous_state);
             match store_state(updated_state) {
                 Ok(()) => println!("All is well."),
                 Err(e) => eprintln!("Error while storing the app state {:?}", e)
@@ -92,66 +52,19 @@ fn main() {
         Action::REMOTE => {
             let podcasts = select_podcasts(settings.podcasts, args.podcast_id);
             let count = args.count.unwrap_or(5);
-            for podcast in podcasts.into_iter() {
-                match get_episodes(&podcast.url, count) {
-                    Ok(episodes) => {
-                        println!("\n{}:", podcast.id.magenta().bold());
-                        for episode in episodes.iter() {
-                            println!("{}", episode);
-                        }
-                    },
-                    Err(e) => {
-                        eprintln!("Could not get the podcast feed: {}", e);
-                    },
-                }
-            }
+            show_remote(podcasts, count);
         },
         Action::LOCAL => {
-            let no_episodes: Vec<Episode> = Vec::new();
             let podcasts = select_podcasts(settings.podcasts, args.podcast_id);
             let count = args.count.unwrap_or(5);
-            for podcast in podcasts.into_iter() {
-                let episodes: Vec<&Episode> = previous_state.get(&podcast.id).unwrap_or(&no_episodes).iter().take(count).collect();
-                if episodes.len() == 0 {
-                    println!("\n{}: no episode available. Download it first.", podcast.id.magenta().bold());
-                } else {
-                    println!("\n{}:", podcast.id.magenta().bold());
-                    for episode in episodes.into_iter() {
-                        println!("{}", episode);
-                    }
-                }
-            }
+            show_local(podcasts, count, previous_state);
         },
         Action::PLAY => {
-            let no_episodes: Vec<Episode> = Vec::new();
             let podcasts = select_podcasts(settings.podcasts, args.podcast_id);
             let media_dir = Path::new(&settings.config.media_dir);
             let count = args.count.unwrap_or(1);
             let player = settings.config.player;
-            for podcast in podcasts.into_iter() {
-                let episodes: Vec<&Episode> = previous_state.get(&podcast.id).unwrap_or(&no_episodes).iter().take(count).collect();
-                let dir_path = media_dir.join(&podcast.id);
-                if episodes.len() == 0 {
-                    println!("\n{}: no episode available. Download it first.", podcast.id.magenta().bold());
-                } else {
-                    println!("\n{}:", podcast.id.magenta().bold());
-                    for episode in episodes.into_iter() {
-                        match get_episode_download(episode, &dir_path) {
-                            Some(path) => {
-                                let child = Command::new(&player)
-                                    .arg(path.display().to_string())
-                                    .stdout(Stdio::piped())
-                                    .spawn()
-                                    .expect("failed to execute the player");
-                                child
-                                    .wait_with_output()
-                                    .expect("failed to wait on child");
-                            },
-                            None => eprintln!("Could not get the file for the episode at URL {}", episode.url)
-                        }
-                    }
-                }
-            }
+            play_podcasts(podcasts, count, media_dir, player, previous_state);
         }
     }
 }
