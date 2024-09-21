929d54fafbedd974d0e679a8532368d471b2e77a
Salil Wadnerkar <swadnerkar@atlassian.com>
2024-09-21 08:04:50 -0700

    Replaced argh with pico-args to make the command line parsing simpler

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,72 @@
 mod podcaster;
-mod args;
 use {
     crate::{
-        args::*,
         podcaster::*
     },
 };
 
+const HELP: &str = "\
+podcaster
+
+USAGE:
+  podcaster [OPTIONS] [ACTION]
+
+FLAGS:
+  -h, --help            Prints help information
+
+OPTIONS:
+  -p String             podcast ID
+  -c NUMBER             count of episodes
+
+ARGS:
+  ACTION                Supported actions are: download, list, catchup, and play. The default action is list.
+";
+
+
+#[derive(Debug)]
+pub struct Args {
+    podcast_id: Option<String>,
+    count: Option<usize>,
+    action: Option<String>,
+}
+
+fn parse_args() -> Result<Args, pico_args::Error> {
+    let mut pargs = pico_args::Arguments::from_env();
+
+    // Help has a higher priority and should be handled separately.
+    if pargs.contains(["-h", "--help"]) {
+        print!("{}", HELP);
+        std::process::exit(0);
+    }
+
+    let args: Args = Args {
+        podcast_id: pargs.opt_value_from_str("-p")?,
+        count: pargs.opt_value_from_str("-c")?,
+        action: pargs.opt_free_from_str()?
+    };
+
+    Ok(args)
+}
 
 fn main() {
-    let args: Args = argh::from_env();
-    if args.version {
-        println!("podcaster {}", env!("CARGO_PKG_VERSION"));
-        return;
-    }
+    let args = parse_args().expect("Failed to parse args");
     let mut podcaster = Podcaster::new();
 
-    match args.action {
-        Action::DOWNLOAD => {
+    match args.action.unwrap_or("list".to_string()).as_str() {
+        "download" => {
             match podcaster.download(args.podcast_id, args.count) {
                 Ok(()) => println!("All is well."),
                 Err(e) => eprintln!("Error while downloading podcasts {:?}", e)
             }
         },
-        Action::LIST => {
-            podcaster.list(args.podcast_id, args.count);
-        },
-        Action::CATCHUP => {
+        "catchup" => {
             podcaster.catchup(args.podcast_id, args.count);
         },
-        Action::PLAY => {
+        "play" => {
             podcaster.play(args.podcast_id, args.count);
-        }
+        },
+        "list" | _ => {
+            podcaster.list(args.podcast_id, args.count);
+        },
     }
 }
