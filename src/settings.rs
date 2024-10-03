576487384b223b0f3195c3d8ff7b63f4c1665089
Salil Wadnerkar <swadnerkar@atlassian.com>
2024-10-03 12:07:44 -0700

    config dir create

--- src/settings.rs
+++ src/settings.rs
@@ -14,6 +14,7 @@ impl Settings {
     pub fn new() -> Result<Settings, Box<dyn Error>> {
         let mut config_path = dirs::config_dir().unwrap();
         config_path.push("podcaster-rs");
+        fs::create_dir_all(&config_path).expect("Failed to create directory for the podcaster config");
         config_path.push("podcasts.toml");
         let contents = fs::read_to_string(config_path)?;
         let config: Settings = toml::from_str(&contents)?;
